02fa60cd6e239744df82abf274e9965924a65d9b
catlomao <84031819+catlomao@users.noreply.github.com>
2025-05-16 21:52:32 +0300

    Update linux.py

--- ezdaemon/linux.py
+++ ezdaemon/linux.py
@@ -1,33 +1,32 @@
-from os import system
-
-#       !<<<<----- LINUX ----->>>>!
-class linux:
-    r"""  
+r"""  
 ## Run Programs in the Background in LINUX!
 ##        🐧🐧🐧🐧🐧🐧🐧🐧🐧🐧
     """
-    def default(Program):
+from os import system
+
+#       !<<<<----- LINUX ----->>>>!
+def default(Program):
         r"""# method 1 --> default"""
 
         system(f"{Program} &")
 
         # <----- 1 ----->
 
-    def nohup(Program):
+def nohup(Program):
         """# method 2 --> nohup"""
 
         system(f"nohup {Program} &>/dev/null &")
 
         # <----- 2 ----->
 
-    def disown(Program):
+def disown(Program):
         """# method 3 --> disown"""
 
         system(f"{Program} & disown")
 
         # <----- 3 ----->
 
-    def setsid(Program):
+def setsid(Program):
         """# method 4 --> setsid"""
 
         system(f"setsid {Program} &>/dev/null &")
