12a995cc8697f1ae4e227d838896485f05343473
catlomao <84031819+catlomao@users.noreply.github.com>
2025-05-16 21:52:49 +0300

    Update windows.py

--- ezdaemon/windows.py
+++ ezdaemon/windows.py
@@ -1,19 +1,19 @@
-from os import system
-
-#       !<<<<----- WINDOWS ----->>>>!
-class win:
-    r"""
+r"""
 ## Run Programs in the Background in WINDOWS!
     ## 🪟🪟🪟🪟
     """
-    def powershell(Program):
+from os import system
+
+#       !<<<<----- WINDOWS ----->>>>!
+
+def powershell(Program):
         r"""# method 1 --> powershell"""
 
         system(f'Start-Process "{Program}" -WindowStyle Hidden')
 
         # <----- 1 ----->
 
-    def cmd(Program):
+def cmd(Program):
         r"""# method 2 --> cmd"""
 
         system(f'start /B "{Program}"')
