313562f979e5ed2a0c4664ab97dd59291c3427b9
catlomao <84031819+catlomao@users.noreply.github.com>
2025-05-18 21:59:54 +0300

    Update unix.py

--- ezdaemon/unix.py
+++ ezdaemon/unix.py
@@ -8,14 +8,14 @@ from os import system
 
 def default(Program):
         r"""## works with any UNIX-based"""
-
+#region default
         system(f"{Program} &")
         # <----- 1 ----->
-
 
 def disown(Program):
         """# method 2 --> disown"""
 # doesn't shit itself and exit
+#region disown
         system(f"{Program} & disown")
 
         # <----- 2 ----->
