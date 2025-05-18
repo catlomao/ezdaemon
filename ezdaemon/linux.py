3a7ad665718a145ee2434ff91cc15e95bb398ab2
catlomao <84031819+catlomao@users.noreply.github.com>
2025-05-18 21:59:37 +0300

    Update linux.py

--- ezdaemon/linux.py
+++ ezdaemon/linux.py
@@ -7,28 +7,28 @@ from os import system
 #       !<<<<----- LINUX ----->>>>!
 def default(Program):
         r"""# method 1 --> default"""
-
+#region default
         system(f"{Program} &")
 
         # <----- 1 ----->
 
 def nohup(Program):
         """# method 2 --> nohup"""
-
+#region nohup
         system(f"nohup {Program} &>/dev/null &")
 
         # <----- 2 ----->
 
 def disown(Program):
         """# method 3 --> disown"""
-
+#region disown
         system(f"{Program} & disown")
 
         # <----- 3 ----->
 
 def setsid(Program):
         """# method 4 --> setsid"""
-
+#region setsid
         system(f"setsid {Program} &>/dev/null &")
 
         # <----- 4 ----->
